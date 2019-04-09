89c4f46dd33d314a9e8744e2d7212f3ac010a9c0
Karan Grover <kgrover97@users.noreply.github.com>
2019-04-09 02:53:23 -0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 //Moved this line
 public class HelloWorld {
     public static void main(String[] args) {
+	// Moved the below line 
         // Prints "Hello, World" to the terminal window.
         // Here right Here is another comment
 	
