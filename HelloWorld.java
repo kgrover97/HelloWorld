a2e5cd8cfe3b813f9e5fdf35ce9d2b3f5aeebc09
kgrover97 <grover.karan97@gmail.com>
2019-04-12 14:22:26 -0700

    cleaned up file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,12 @@
-//Moved this line
 public class HelloWorld {
     public static void main(String[] args) {
-	// Moved the below line 
-        // Prints "Hello, World" to the terminal window.
-       
-	// Here right Here is another comment
-	
 	System.out.println("A test line");
-	System.out.println("This is a diff line");
-        System.out.println("Hello, World");
 
         for(int i = 0; i < 6; i++){
 	    //do stuff
+
         }
+
         for(int i = 0; i < 6; i++){
             //do different stuff
         }
