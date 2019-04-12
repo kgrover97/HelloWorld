a9884b09e6dc5dff6c8937379f3be5a6d1cd60f0
kgrover97 <grover.karan97@gmail.com>
2019-04-12 14:31:31 -0700

    White space clean up

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-	System.out.println("A test line");
+        System.out.println("A test line");
 
         for(int i = 0; i < 6; i++){
-	    //do stuff
-
+            //do stuff
         }
 
         for(int i = 0; i < 6; i++){
